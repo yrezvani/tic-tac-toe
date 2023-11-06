f9941357605a073bad8b045531c4f3ee8122bd2c
SharpLad <143443228+yrezvani@users.noreply.github.com>
2023-11-06 15:41:58 +0000

    Add checkWin function. remove board background.

--- script.js
+++ script.js
@@ -1,41 +1,68 @@
 const gameBoard = (function () {
-    let xPlays = [];
-    let oPlays = [];
-    let xTurn = true;
-    gameboard = []
-    const windCombs = [
+    const createPlayer = function (name, marker, activePlayer) {
+        return { name, marker, activePlayer };
+    };
+
+    const player1 = createPlayer('player 1', 'X', true);
+    const player2 = createPlayer('player 2', 'O', false);
+
+    const board = [];
+
+    let gameWinner = false;
+
+    const winCombs = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
         [0, 3, 6],
         [1, 4, 7],
         [2, 5, 8],
-        [0, 4, 9]
+        [0, 4, 8],
+        [2, 4, 6],
     ];
+
     const cells = document.querySelectorAll('.cell');
-    
 
-
-    
     cells.forEach(cell => {
-    addEventListener('click', function (e) {
-        const cell = e.target;
-        text = cell.textContent;
+        addEventListener('click', function (e) {
+            const cell = e.target;
+            text = cell.textContent;
 
-        if (xTurn && !text) {
-            cell.textContent = 'x';
-            gameboard.splice(cell.dataset.no, 0, 'x');
-            xTurn = false
+            if (player1.activePlayer && !text && !gameWinner) {
+                cell.textContent = 'x';
+                board[cell.dataset.no] = 'x';
+                player1.activePlayer = false;
+                player2.activePlayer = true;
+                if (checkWin('x')) {
+                    showWinner('x');
+                    resetGame();
+                }
+            } else if (player2.activePlayer && !text && !gameWinner) {
+                cell.textContent = 'o';
+                board[cell.dataset.no] = 'o';
 
-        } else if (!xTurn && !text) {
-            cell.textContent = 'o';
-            gameboard.splice(cell.dataset.no, 0, 'x');
-            xTurn = true
-        } else return;
+                player2.activePlayer = false;
+                player1.activePlayer = true;
+                if (checkWin('o')) {
+                    showWinner('o');
+                    resetGame();
+                }
+            } else return;
+        });
+    });
 
-    }, );
-});
+    for (let i = 0; i < board.length; i++) {
+        if (board[i] === 'x') xPlays.push(i);
+        if (board[i] === 'o') oPlays.push(i);
+    }
 
-
+    return { board, player1, player2, oPlays, xPlays, winCombs };
 })();
 
+const checkWin = function (marker) {
+    return gameBoard.winCombs.some(comb => {
+        return comb.every(index => {
+            return gameBoard.board[index] === marker;
+        });
+    });
+};
